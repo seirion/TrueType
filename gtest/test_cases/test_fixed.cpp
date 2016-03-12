31c78dd72cadba1c654e1359d2aa1bfea0132ba6
seirion <seirion@mensakorea.org>
2016-03-12 16:37:37 +0900

    test cases for Fixed

---
+++ gtest/test_cases/test_fixed.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "types/Fixed.h"
+
+GTEST_TEST(Fixed, version) {
+    Fixed version(1, 0);
+    EXPECT_EQ(version.toString(), "1.0");
+
+    Fixed negative(-1, 15);
+    EXPECT_EQ(negative.toString(), "-1.15");
+}
