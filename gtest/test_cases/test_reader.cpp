abf4259296b2a51540201c11c6cd6ed8e2ca0048
seirion <seirion@mensakorea.org>
2016-03-14 21:34:55 +0900

    add test cases for testing reading of the head table

--- gtest/test_cases/test_reader.cpp
+++ gtest/test_cases/test_reader.cpp
@@ -34,23 +34,21 @@ TEST_F(FontReadingTest, head) {
     const head &head = font.getHead();
 
     EXPECT_EQ(head.getVersion().toString(), "1.0");
-    EXPECT_EQ(head.getFontRevision().toString(), "6.90");
-    /* FIXME
-    EXPECT_EQ(head.getCheckSumAdjustment(), 0);
-    EXPECT_EQ(head.getMagicNumber(), 0);
-    EXPECT_EQ(head.getFlags(), 0);
-    EXPECT_EQ(head.getUnitsPerEm(), 0);
-    EXPECT_EQ(head.getCreated(), 0);
-    EXPECT_EQ(head.getModified(), 0);
-    EXPECT_EQ(head.getXMin(), 0);
-    EXPECT_EQ(head.getYMin(), 0);
-    EXPECT_EQ(head.getXMax(), 0);
-    EXPECT_EQ(head.getYMax(), 0);
+    EXPECT_EQ(head.getFontRevision().toString(), "6.9");
+    EXPECT_EQ(head.getCheckSumAdjustment(), 0xfd9ba091);
+    EXPECT_EQ(head.getMagicNumber(), 0x5f0f3cf5);
+    EXPECT_EQ(head.getFlags(), 0x081b);
+    EXPECT_EQ(head.getUnitsPerEm(), 2048);
+    EXPECT_EQ(head.getCreated(), 0xa2e3272aLL);
+    EXPECT_EQ(head.getModified(), 0xd19de3fdLL);
+    EXPECT_EQ(head.getXMin(), -1361);
+    EXPECT_EQ(head.getYMin(), -665);
+    EXPECT_EQ(head.getXMax(), 4096);
+    EXPECT_EQ(head.getYMax(), 2129);
     EXPECT_EQ(head.getMAcStyle(), 0);
-    EXPECT_EQ(head.getLowestRecPPEM(), 0);
-    EXPECT_EQ(head.getFontDirectionHint(), 0);
-    EXPECT_EQ(head.getIndexToLocFormat(), 0);
+    EXPECT_EQ(head.getLowestRecPPEM(), 9);
+    EXPECT_EQ(head.getFontDirectionHint(), 1);
+    EXPECT_EQ(head.getIndexToLocFormat(), 1);
     EXPECT_EQ(head.getGlyphDataFormat(), 0);
-    */
 }
 
